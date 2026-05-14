89e93c0efa9cb7bcf5d069cdaa8376dd905b98cf
OldWooood <deatrg@foxmail.com>
2026-05-14 22:10:32 +0800

    chore: bump version to 1.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.deatrg.dnsfilter"
         minSdk = 29
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.3.3"
+        versionCode = 17
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
