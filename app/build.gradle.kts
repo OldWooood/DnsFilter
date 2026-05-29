22afa8e01ed0d38e146035c2af363e59eb3df6e3
OldWooood <deatrg@foxmail.com>
2026-05-29 20:25:32 +0800

    chore: bump version to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.deatrg.dnsfilter"
         minSdk = 29
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.7.0"
+        versionCode = 25
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
